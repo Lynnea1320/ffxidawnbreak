02e2f02adeb7bde7f275c1172ca07afa52db80c0
Brian Silvi <lunaofthemoon@gmail.com>
2022-01-25 12:59:14 +0100

    Adjusted permissions on all the GM commands to match old server
    Also added the world chat command.

--- scripts/commands/delmission.lua
+++ scripts/commands/delmission.lua
@@ -1,13 +1,29 @@
------------------------------------
+---------------------------------------------------------------------------------------------------
 -- func: delmission <logID> <missionID> <player>
 -- desc: Deletes the given mission from the GM or target player.
------------------------------------
+
+-- Mission Log ID	Mission Log Name	Full Name
+-- 			0		SANDORIA			Sandoria
+-- 			1		BASTOK				Bastok
+-- 			2		WINDURST			Windurst
+-- 			3		ZILART				Rise of the Zilart
+-- 			4		TOAU				Treasures of Aht Urhgan
+-- 			5		WOTG				Wings of the Goddess
+-- 			6		COP					Chains of Promathia
+-- 			7		ASSAULT				Assault
+-- 			8		CAMPAIGN			Campaign
+-- 			9		ACP					A Crystalline Prophecy
+-- 			10		AMK					A Moogle Kupo d'Etat
+-- 			11		ASA					A Shantotto Ascension
+-- 			12		SOA					Seekers of Adoulin
+-- 			13		ROV					Rhapsodies of Vana'diel
+---------------------------------------------------------------------------------------------------
 
 require("scripts/globals/missions")
 
 cmdprops =
 {
-    permission = 1,
+    permission = 3,
     parameters = "sss"
 }
 
---
+++ scripts/commands/world.lua
@@ -0,0 +1,33 @@
+---------------------------------------------------------------------------------------------------
+-- func: w, world
+-- desc: prints a message to the world
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 0,
+    parameters = "s"
+}
+
+function error(player, msg)
+    player:PrintToPlayer(msg)
+    player:PrintToPlayer("!world <message>")
+end
+
+function onTrigger(player, msg)
+    if msg == nil then
+        error(player, "Cannot send an empty message.")
+    else
+        if player:getGMLevel() == 2 then
+            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[GM]%s", player:getName()))
+        elseif player:getGMLevel() == 3 then
+            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[SGM]%s", player:getName()))
+        elseif player:getGMLevel() == 4 then
+            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[LGM]%s", player:getName()))
+        elseif player:getGMLevel() == 5 then
+            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, string.format("[SO]%s", player:getName()))
+        else
+            player:PrintToArea(string.format("%s", msg), tpz.msg.channel.YELL, tpz.msg.area.SERVER, player:getName())
+        end
+    end
+end
