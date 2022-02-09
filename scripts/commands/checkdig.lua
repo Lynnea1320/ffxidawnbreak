f46a4dd08a9a4a6886828f0e091807a01efae44a
Brian Silvi <lunaofthemoon@gmail.com>
2022-02-09 15:56:42 +0100

    Added command to check choco dig skill

---
+++ scripts/commands/checkdig.lua
@@ -0,0 +1,17 @@
+-----------------------------------
+-- func: checkdig
+-- desc: Checks your chocobo digging skill level.
+-----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/msg")
+--------------------------------
+
+cmdprops =
+{
+    permission = 0,
+    parameters = ""
+}
+
+function onTrigger(player)
+    player:PrintToPlayer(string.format("Your chocobo digging skill is %i.", player:getCharSkillLevel(xi.skill.DIG)/10), xi.msg.channel.SYSTEM_3)
+end
